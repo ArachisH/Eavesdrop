4368b3bdb66972675a87dbefca2c3b3ac04a0fe5
ArachisH <iKBenPeanut@gmail.com>
2017-11-04 13:40:56 -0700

    Yup

--- Eavesdrop/Properties/AssemblyInfo.cs
+++ Eavesdrop/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Eavesdrop")]
 [assembly: AssemblyCopyright("Copyright © ArachisH 2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("The Old Nut Man")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
