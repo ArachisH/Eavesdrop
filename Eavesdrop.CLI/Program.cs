7e064e43437ea28a7fdc1386376d2bd9c3a1e0ad
ArachisH <iKBenPeanut@gmail.com>
2022-01-16 19:16:37 -0800

    Changed framework target to .NET 6

---
+++ Eavesdrop.CLI/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Eavesdrop.CLI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (Eavesdropper.Certifier.CreateTrustedRootCertificate())
+            {
+                Eavesdropper.RequestInterceptedAsync += Eavesdropper_RequestInterceptedAsync;
+                Eavesdropper.ResponseInterceptedAsync += Eavesdropper_ResponseInterceptedAsync;
+
+                Eavesdropper.Initiate(8686, Interceptors.Default);
+                Console.WriteLine("Press any key to terminate the application at any time...");
+            }
+
+            Console.ReadKey();
+            Eavesdropper.Terminate();
+        }
+
+        private static Task Eavesdropper_RequestInterceptedAsync(object sender, RequestInterceptedEventArgs e)
+        {
+            Console.WriteLine("Requesting: " + e.Uri);
+            return Task.CompletedTask;
+        }
+        private static Task Eavesdropper_ResponseInterceptedAsync(object sender, ResponseInterceptedEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
