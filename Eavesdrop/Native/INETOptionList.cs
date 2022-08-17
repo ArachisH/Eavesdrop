52765eae5bded0c1b71a5b6a15669f6ea1811734
Paulus Pärssinen <paulus.parssinen@gmail.com>
2022-08-18 02:09:52 +0300

    Rewrite wininet interop (#9)

---
+++ Eavesdrop/Native/INETOptionList.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Eavesdrop;
+
+/// <summary>
+/// Contains the list of options for a particular Internet connection.
+/// </summary>
+/// <remarks>INTERNET_PER_CONN_OPTION_LIST structure (wininet.h)</remarks>
+[StructLayout(LayoutKind.Sequential)]
+internal unsafe struct INETOptionList
+{
+    /// <summary>
+    /// Size of the structure, in bytes.
+    /// </summary>
+    public int Size;
+
+    /// <summary>
+    /// Pointer to a string that contains the name of the RAS connection or NULL, which indicates the default or LAN connection, to set or query options on.
+    /// </summary>
+    public char* Connection;
+
+    /// <summary>
+    /// Number of options to query or set.
+    /// </summary>
+    public int OptionCount;
+
+    /// <summary>
+    /// Options that failed, if an error occurs.
+    /// </summary>
+    public int OptionError;
+
+    /// <summary>
+    /// Pointer to an array of <see cref="INETOption"/> structures containing the options to query or set.
+    /// </summary>
+    public INETOption* OptionsPtr;
+}
---
+++ Eavesdrop/Native/NativeMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Eavesdrop;
+
+// TODO-FUTURE: .NET 7 LibraryImport generator
+internal unsafe static class NativeMethods
+{
+    /// <summary>
+    /// Sets an Internet option.
+    /// </summary>
+    /// <param name="hInternet">Handle on which to set information.</param>
+    /// <param name="dwOption">Internet option to be set. This can be one of the <see cref="OptionKind"/> values.</param>
+    /// <param name="lpBuffer">Pointer to a buffer that contains the option setting.</param>
+    /// <param name="dwBufferLength">Size of the lpBuffer buffer.</param>
+    /// <returns>Returns <c>true</c> if successful, or <c>false</c> otherwise. To get a specific error message, call <see cref="Marshal.GetLastWin32Error"/>.</returns>
+    [DllImport("wininet.dll", EntryPoint = "InternetSetOptionW", SetLastError = true, CharSet = CharSet.Unicode)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool InternetSetOption(void* hInternet, int dwOption, void* lpBuffer, int dwBufferLength);
+}
