ffda3723c1a1bdce765b242781a19a977ecb5732
PaulusParssinen <paulus.parssinen@gmail.com>
2022-08-23 13:32:18 +0300

    Use UTF-8 string literals from C# 11

--- Eavesdrop/Network/EavesNode.cs
+++ Eavesdrop/Network/EavesNode.cs
@@ -16,10 +16,9 @@ public sealed class EavesNode : IDisposable
     private static readonly HttpMethod _connectMethod = new("CONNECT");
     private static readonly HttpResponseMessage _okResponse = new(HttpStatusCode.OK);
 
-    // TODO-FUTURE: In C# 11, use raw string literals
-    private static ReadOnlySpan<byte> _eolBytes => new byte[2] { (byte)'\r', (byte)'\n' };
-    private static ReadOnlySpan<byte> _eofBytes => new byte[4] { (byte)'\r', (byte)'\n', (byte)'\r', (byte)'\n' };
-    private static ReadOnlySpan<byte> _connectBytes => new byte[7] { (byte)'C', (byte)'O', (byte)'N', (byte)'N', (byte)'E', (byte)'C', (byte)'T' };
+    private static ReadOnlySpan<byte> _eolBytes => "\r\n"u8;
+    private static ReadOnlySpan<byte> _eofBytes => "\r\n\r\n"u8;
+    private static ReadOnlySpan<byte> _connectBytes => "CONNECT"u8;
 
     private readonly Socket _client;
     private readonly CertificateManager _certifier;
