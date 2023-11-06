c20872ab1c347f5e87acd6ac1f8dd8a348f6f811
Adolfo Huitron <adolfo@huitron.dev>
2023-11-06 00:12:44 -0800

    refactor: Add custom IWebProxy wrapper that allows the proxy server to bypass itself

---
+++ Eavesdrop/Network/SelfBypassWebProxy.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace Eavesdrop.Network;
+
+internal sealed class SelfBypassWebProxy : IWebProxy
+{
+    public IWebProxy? Proxy { get; set; }
+
+    public ICredentials? Credentials
+    {
+        get => Proxy?.Credentials;
+        set
+        {
+            if (Proxy != null)
+            {
+                Proxy.Credentials = value;
+            }
+        }
+    }
+
+    public Uri? GetProxy(Uri destination) => Proxy?.GetProxy(destination);
+    public bool IsBypassed(Uri host) => Proxy == null || Proxy.IsBypassed(host) || Proxy.GetProxy(host) == host;
+}
