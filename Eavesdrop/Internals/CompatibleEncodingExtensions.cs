71bf5547dfdb0f54a424f27c2468df42a2792009
Adolfo Huitron <adolfo@huitron.dev>
2024-03-13 17:08:51 -0700

    refactor: add type to provide extensions for older framework targets

---
+++ Eavesdrop/Internals/CompatibleEncodingExtensions.cs
@@ -0,0 +1,31 @@
+﻿#if NETSTANDARD2_0
+using System.Text;
+using System.Buffers;
+
+namespace Eavesdrop;
+
+internal static class CompatibleEncodingExtensions
+{
+    public static unsafe int GetBytes(this Encoding encoding, ReadOnlySpan<char> chars, Span<byte> buffer)
+    {
+        fixed (char* charsPtr = chars)
+        fixed (byte* bufferPtr = buffer)
+        {
+            return encoding.GetBytes(charsPtr, chars.Length, bufferPtr, buffer.Length);
+        }
+    }
+    public static unsafe long GetBytes(this Encoding encoding, string value, IBufferWriter<byte> writer)
+    {
+        int byteCount = encoding.GetByteCount(value);
+        Span<byte> buffer = writer.GetSpan(byteCount);
+
+        fixed (char* valuePtr = value)
+        fixed (byte* bufferPtr = buffer)
+        {
+            int written = encoding.GetBytes(valuePtr, value.Length, bufferPtr, buffer.Length);
+            writer.Advance(written);
+            return written;
+        }
+    }
+}
+#endif
