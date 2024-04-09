3522aec02eefefe6fa0f14b2e6692f5cff53f8af
Adolfo Huitron <adolfo@huitron.dev>
2024-04-08 22:33:29 -0700

    chore: remove unused types
    Superseded by types in Certificates namespace

--- Eavesdrop/ICertifier.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Eavesdrop;
-
-public interface ICertifier
-{
-    X509Certificate2? GenerateCertificate(string certificateName);
-}
