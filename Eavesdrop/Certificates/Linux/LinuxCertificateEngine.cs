6945f8b1d9c6c4562e5ed86916f66b5e7e46e3d9
Adolfo Huitron <adolfo@huitron.dev>
2026-04-04 17:14:24 -0700

    chore: add non-implemented type for Linux support

---
+++ Eavesdrop/Certificates/Linux/LinuxCertificateEngine.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Eavesdrop.Certificates.Linux;
+
+public sealed class LinuxCertificateEngine : ICertificateEngine
+{
+    private bool _disposed;
+
+    public X509Certificate2 CreateSelfSigned(string name, string issuerName, DateTime notBefore, DateTime notAfter)
+    {
+        throw new NotImplementedException();
+    }
+    public X509Certificate2 Create(string name, X509Certificate2 issuerCertificate, DateTime notBefore, DateTime notAfter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+    private void Dispose(bool disposing)
+    {
+        if (!disposing || _disposed) return;
+        // TODO: Disposed managed objects.
+        _disposed = true;
+    }
+}
