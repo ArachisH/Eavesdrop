319b8ae38e315691069bddcf347e59754763aeac
Adolfo Huitron <adolfo@huitron.dev>
2026-04-04 17:51:38 -0700

    refactor: allow specifying installing root CA into trusted store or not
    Rename TrustedRootCA > RootCertificateAuthority
    Add IsRootCATrusted boolean property
    
    Rename parameter issuerName to organization
    Rename parameter name to commonName

--- Eavesdrop/Certificates/CertificateProvider.cs
+++ Eavesdrop/Certificates/CertificateProvider.cs
@@ -17,7 +17,8 @@ public sealed class CertificateProvider : IDisposable
     public DateTime NotAfter { get; set; } = DateTime.Now.AddMonths(1);
 
     public ICertificateEngine Engine { get; }
-    public X509Certificate2? TrustedRootCA { get; private set; }
+    public bool IsRootCATrusted { get; private set; }
+    public X509Certificate2? RootCertificateAuthority { get; private set; }
 
     public bool IsDisposingCertificateEngine { get; set; } = true;
     public bool IsDisposingCachedCertificates { get; set; } = true;
@@ -36,43 +37,43 @@ public sealed class CertificateProvider : IDisposable
     public X509Certificate2? IssueCertificate(string name)
     {
         return _issuedCertificates.GetOrAdd(name, CreateCertificate);
-        }
+    }
     private X509Certificate2 CreateCertificate(string name)
-        {
+    {
         return RootCertificateAuthority != null
             ? Engine.Create(name, RootCertificateAuthority, NotBefore, NotAfter)
             : throw new InvalidOperationException("Must first add a self-signed certificate authority to the root store before issuing any certificates.");
-            }
-
-
-
-    public bool TryCreateTrustedRootCA(string name, string issuerName)
-    {
-        string subject = $"CN={name}, O={issuerName}";
-        if (TrustedRootCA != null && TrustedRootCA.HasPrivateKey && subject == TrustedRootCA.Subject) return true;
-
-        X509Certificate2? trustedRootCA = GetTrustedRootCAFromStore(subject);
-        if (trustedRootCA != null && DateTime.Now >= trustedRootCA.NotAfter)
-        {
-            if (TryRemoveCertificateFromRootStore(trustedRootCA))
-            {
-                trustedRootCA = null;
-            }
-        }
-
-        if (trustedRootCA == null)
-        {
-            X509Certificate2 untrustedRootCA = Engine.CreateSelfSigned(name, issuerName, NotBefore, NotAfter);
-            if (TryAddCertificateToRootStore(untrustedRootCA))
-            {
-                trustedRootCA = untrustedRootCA;
-            }
-        }
-
-        TrustedRootCA = trustedRootCA;
-        return TrustedRootCA != null;
     }
-    public bool TryCreateTrustedRootCA(string issuerName) => TryCreateTrustedRootCA($"{issuerName} Root Certificate Authority", issuerName);
+
+    public bool TryCreateTrustedRootCA(string organization, string commonName, bool addToUserStore = true)
+    {
+        string subject = $"CN={commonName}, O={organization}";
+        if (RootCertificateAuthority != null && RootCertificateAuthority.HasPrivateKey && subject == RootCertificateAuthority.Subject) return true;
+
+        X509Certificate2? rootCA = GetTrustedRootCAFromStore(subject);
+        if (rootCA != null) // Check for existing trusted CA
+        {
+            // Ensure CA is still valid
+            IsRootCATrusted = DateTime.Now < rootCA.NotAfter;
+            if (!IsRootCATrusted)
+            {
+                // Remove expired/invalid CA
+                _ = TryRemoveCertificateFromRootStore(rootCA);
+                rootCA = null;
+            }
+        }
+
+        // Attempt to generate, and add CA to trusted user store if necessary.
+        if (rootCA == null)
+        {
+            rootCA = Engine.CreateSelfSigned(commonName, organization, NotBefore, NotAfter);
+            IsRootCATrusted = addToUserStore && TryAddCertificateToRootStore(RootCertificateAuthority);
+        }
+
+        RootCertificateAuthority = rootCA;
+        return RootCertificateAuthority != null;
+    }
+    public bool TryCreateTrustedRootCA(string organization) => TryCreateTrustedRootCA(organization, $"{organization} Root Certificate Authority");
 
     private static ICertificateEngine GetCertificateEngine()
     {
@@ -131,20 +132,20 @@ public sealed class CertificateProvider : IDisposable
     {
         if (!disposing || _disposed) return;
 
-                if (IsDisposingCertificateEngine)
-                {
-                    Engine.Dispose();
-                }
-                TrustedRootCA?.Dispose();
-
-                if (IsDisposingCachedCertificates)
-                {
-                    foreach (X509Certificate2 cachedCertificate in _issuedCertificates.Values)
-                    {
-                        cachedCertificate.Dispose();
-                    }
-                }
-                _issuedCertificates.Clear();
-            _disposed = true;
+        if (IsDisposingCertificateEngine)
+        {
+            Engine.Dispose();
         }
+        RootCertificateAuthority?.Dispose();
+
+        if (IsDisposingCachedCertificates)
+        {
+            foreach (X509Certificate2 cachedCertificate in _issuedCertificates.Values)
+            {
+                cachedCertificate.Dispose();
+            }
+        }
+        _issuedCertificates.Clear();
+        _disposed = true;
     }
+}
